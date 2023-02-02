42ceadf4930200999fb39e9d8acb0389f10f30ac
iirtrtrt <tk@tk>
2023-02-02 20:53:58 +0200

    exercise h thread done

---
+++ hello/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // println!("Hello, world!");
+    let mut bunnies = 4;
+    // let mut bunnies: i32 = 4;
+    // let mut bunnies = 4_i32;
+    // let mut bunnies = 4i32;
+    // let (bunnies, carrots) = (50, 8);
+    println!("{}", bunnies);
+    bunnies = 2;
+    println!("{}", bunnies);
+}
+
+// fn main() {
+//     let x = 10;
+//     {
+//         let y = 11;
+//         println!("{}, {}", x, y);
+//     }
+//     println!("{}, {}", x, x);
+// }
