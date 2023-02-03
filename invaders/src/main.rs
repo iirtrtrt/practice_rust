68dacae47db9682af667023c4fdf62f8c2360b00
iirtrtrt <tk@tk>
2023-02-03 23:28:53 +0200

    make a moving space ship

---
+++ invaders/src/main.rs
@@ -0,0 +1,80 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, ExecutableCommand};
+use invaders::frame::{new_frame, Drawable};
+use invaders::player::Player;
+use invaders::{frame, render};
+// use rusty_audio::Audio;
+use std::error::Error;
+use std::sync::mpsc;
+use std::time::Duration;
+use std::{io, thread};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // let mut audio = Audio::new();
+    // audio.add("explode", "../sounds/explode.wav");
+    // audio.add("lose", "../sounds/lose.wav");
+    // audio.add("move", "../sounds/move.wav");
+    // audio.add("pew", "../sounds/pew.wav");
+    // audio.add("startup", "../sounds/startup.wav");
+    // audio.add("win", "../sounds/win.wav");
+    // audio.play("startup");
+
+    let mut stdout = io::stdout();
+    // ? -> crash if it has an error
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a seperate thread
+    let (render_tx, render_rx) = mpsc::channel::<Vec<Vec<&str>>>();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
+    let mut player = Player::new();
+    'gameloop: loop {
+        // Per-frame init
+        let mut curr_frame = new_frame();
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw & render
+        player.draw(&mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    // audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    return Ok(());
+}
